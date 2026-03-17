3d05834b226d8ef2bb8b71ee10537079be242f26
Nicolas Huici <111755754+nicodigo@users.noreply.github.com>
2026-03-17 18:53:09 -0300

    aniado un signo de excl

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 def main():
     codigo = requests.get("https://www.unlu.edu.ar/").status_code
     print(codigo)
-    print("Hola mundo desde docker!")
+    print("Hola mundo desde docker!!")
 
 
 if __name__ == "__main__":
